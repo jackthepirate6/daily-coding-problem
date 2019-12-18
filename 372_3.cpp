779fbdac33d1179854efd266e0f1c6d4b90a1bba
Ajinkya Narnaware <narnawareajinkya@gmail.com>
2019-12-18 19:44:26 +0530

    Create 372_3.cpp

---
+++ 372_3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace::std;
+
+int nod(int i)
+{
+    string str;
+    str = to_string(i);
+    return(str.size());
+}
+
+int main()
+{
+    cout<<nod(4352)<<endl;
+}
